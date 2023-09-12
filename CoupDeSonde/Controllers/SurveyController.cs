7852769d9515f13ecbd28fd29b8bcf2d8474d98b
Gwenn Durpoix-Espinasson <g.durpoix-espinasson@ecam.fr>
2023-09-12 17:03:14 -0400

    Premier jet GET et checkKey

--- CoupDeSonde/Controllers/SurveyController.cs
+++ CoupDeSonde/Controllers/SurveyController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.Sqlite;
 using System.Data;
+using System.Diagnostics;
+using System.Text;
 
 namespace CoupDeSonde.Controllers
 {
@@ -15,43 +17,104 @@ namespace CoupDeSonde.Controllers
             public int SurveyNumber { get; set; }
             //the answers are concatenated under a string form: 'abcd'
             public string Answers { get; set; }
+            //the questions of the surevy depends on the SurveyNumber
+            public string Question1 { get; }
+            public string Question2 { get; }
+            public string Question3 { get; }
+            public string Question4 { get; }
+
+            public Survey(int number)
+            {
+                SurveyNumber = number;
+                Answers = "";
+                Question1 = "1. À quelle tranche d'âge appartenez-vous? a:0-25 ans, b:25-50 ans, c:50-75 ans, d:75 ans et plus\n\n";
+                Question2 = "2. Êtes-vous une femme ou un homme? a:Femme, b:Homme, c:Je ne veux pas répondre\n\n";
+
+                if (SurveyNumber == 1)
+                {
+                    Question3 = "3. Quel journal lisez-vous à la maison? a:La Presse, b:Le Journal de Montréal, c:The Gazette, d:Le Devoir\n\n";
+                    Question4 = "4. Combien de temps accordez-vous à la lecture de votre journal quotidiennement? a:Moins de 10 minutes; b:Entre 10 et 30 minutes, c:Entre 30 et 60 minutes, d:60 minutes ou plus\n\n";
+                }
+                else
+                {
+                    Question3 = "3. Combien de tasses de café buvez-vous chaque jour? a: Je ne bois pas de café, b:Entre 1 et 5 tasses, c: Entre 6 et 10 tasses, d: 10 tasses ou plus\n\n";
+                    Question4 = "4. Combien de consommations alcoolisées buvez-vous chaque jour? a: 0, b: 1, c: 2 ou 3, d: 3 ou plus\n\n";
+                }
+            }
         }
-        //[HttpGet]
-        //public ActionResult<IEnumerable<string>> GetSurvey()
-        //{
+        /*
+        static string checkKey(string key)
+        {
+            
+            string pathToExe = "Path.exe";
 
-        //}
+            Process process = new Process();
+            process.StartInfo.FileName = "Path.exe";
+            process.StartInfo.Arguments = key;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.Start();
 
+            //read the output of the key checker
+            string output = process.StandardOutput.ReadToEnd();
+
+            return output;
+        }
+        */
+        
+        [HttpGet("getSondage")]
+        public ActionResult<IEnumerable<string>> GetSurvey()
+        {
+            //select and create randomly one of the two surveys
+            Random rand = new Random();
+            int surveyNumber = rand.Next(1, 3);
+            Console.WriteLine($"Submitting survey number {surveyNumber}");
+
+            Survey requested = new Survey(surveyNumber);
+            return Ok(requested);
+        }
+        
+        /*
         [HttpPost("postSondage")]
         public ActionResult<string> PostSurvey([FromBody] Survey survey)
         {
-            //if (HttpContext.Request.Headers.ContainsKey("X-API-KEY"))
-            //{
-            //  var apiKey = HttpContext.Request.Headers["X-API-KEY"];
+            if (HttpContext.Request.Headers.ContainsKey("X-API-KEY"))
+            {
+                string apiKey = HttpContext.Request.Headers["X-API-KEY"];
 
-            //should check whether the key is good or not
+                //should check whether the key is good or not
+                string username = SurveyController.checkKey(apiKey);
 
-            Console.WriteLine("Request received");
-            string dbPath = Path.Combine(Environment.CurrentDirectory, "surveyResults.db");
-            string connString = string.Format("Data Source={0}", dbPath);
-            var connection = new SqliteConnection(connString);
-            connection.Open();
-            Console.WriteLine("Connection to DB established");
+                if (username != "ERR")
+                {
+                    Console.WriteLine("Request received");
 
-            SqliteDataReader sqlite_datareader;
-            SqliteCommand sqlite_cmd;
-            sqlite_cmd = connection.CreateCommand();
-            sqlite_cmd.CommandText = String.Format($"INSERT INTO resultats(SurveyID,Answer)VALUES({survey.SurveyNumber},'{survey.Answers}')");
+                    string dbPath = Path.Combine(Environment.CurrentDirectory, "surveyResults.db");
+                    string connString = string.Format("Data Source={0}", dbPath);
+                    var connection = new SqliteConnection(connString);
+                    connection.Open();
+                    Console.WriteLine("Connection to DB established");
 
-            sqlite_cmd.ExecuteReader();
+                    //SqliteDataReader sqlite_datareader;
+                    SqliteCommand sqlite_cmd;
+                    sqlite_cmd = connection.CreateCommand();
+                    sqlite_cmd.CommandText = String.Format($"INSERT INTO resultats(SurveyID,Answer)VALUES({survey.SurveyNumber},'{survey.Answers}')");
 
-            return "done";
-            //}
-            //else
-            //{
-              //  return "bad key";
-            //}
+                    sqlite_cmd.ExecuteReader();
 
-        }
+                    return Ok("done");
+                }
+                else
+                {
+                    return "not submitted";
+                }
+
+            }
+            else
+            {
+                return "not sumbmitted";
+            }
+
+        }*/
     }
 }
